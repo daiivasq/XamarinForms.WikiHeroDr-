d326a79318a4b7c8ab7d720daca4a85d29cb9644
Daiivasq <daihianav@gmail.com>
2020-03-26 22:48:38 -0400

    add home and viewmodel

---
+++ WikiHero/WikiHero/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WikiHero.Services;
+
+namespace WikiHero.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel
+    {
+        public HomePageViewModel(INavigationService navigationService, IPageDialogService dialogService, ApiComicsVine apiComicsVine):base(navigationService,dialogService, apiComicsVine)
+        {
+            //Animal  si la clase que vas a heredar tiene un maldito contructor que debes de hacer El MALDITO BASE , HICE QUE MI HERMANA ENSE;ARA LA NALGA
+            //  ESTOY ANALIZANDO LA SITUCION NO ERES SANA PARA MI 
+            //AHHHHHAHHAHHHHAH MUERETOOOOOOOOOOOO
+        }
+    }
+}
---
+++ WikiHero/WikiHero/Views/ControlsViews/HomeCarouselView.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WikiHero.Views.ControlsViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeCarouselView : ContentView
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
+            nameof(ItemsSource),
+            typeof(IEnumerable),
+            typeof(HomeCarouselView),
+            propertyChanged: ColletionViewChanged);
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+        public static readonly BindableProperty ColorProperty = BindableProperty.Create(nameof(ColorFrame), typeof(Color), typeof(CardView));
+        public Color ColorFrame
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+        private static void ColletionViewChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is HomeCarouselView control)) return;
+            var items = (IList)newValue;
+            control.publisherList.ItemsSource = items;
+        }
+        public HomeCarouselView()
+        {
+            InitializeComponent();
+        }
+    }
+}
